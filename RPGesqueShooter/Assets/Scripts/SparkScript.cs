67444a1a54f43f3eb426f62ddd29cd3315de37d4
Andrew Day <and0ne808@gmail.com>
2015-11-12 09:12:54 -0700

    Added Spark Effects To Sliders
    Also added sparking sound effect

---
+++ RPGesqueShooter/Assets/Scripts/SparkScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparkScript : MonoBehaviour {
+
+    public ParticleSystem part;
+    public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        part = GetComponent<ParticleSystem>();
+        audio = GetComponent<AudioSource>();
+        part.Stop();
+        part.Clear();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown()
+    {
+        part.Play();
+        Debug.Log("You activated OnMouseDown");
+        audio.Play();
+    }
+
+    void OnMouseUp()
+    {
+        part.Stop();
+        audio.Stop();
+        //part.Clear();
+    }
+
+}
