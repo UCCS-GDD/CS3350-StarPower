e13d0a238657e2759771708953df506303d4bcbe
Andrew Day <and0ne808@gmail.com>
2015-11-24 12:43:37 -0700

    Created 3D Dustfield Effect & Added nebulas
    In level1 of the game

---
+++ RPGesqueShooter/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float scrollSpeed = 1;
+
+    private Transform trans;
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.Translate(new Vector3(0, scrollSpeed * Time.deltaTime * -1, 0));
+	}
+}
