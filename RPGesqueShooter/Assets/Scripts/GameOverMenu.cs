3c38e7c4cb605c9e6451851d0293061e6027c65a
kmayo3 <katlyn.mayo@ymail.com>
2015-11-24 15:34:43 -0700

    Fixed the game buttons
    They now go to the correct places

--- RPGesqueShooter/Assets/Scripts/GameOverMenu.cs
+++ RPGesqueShooter/Assets/Scripts/GameOverMenu.cs
@@ -9,6 +9,20 @@ public class GameOverMenu : MonoBehaviour
         // load the main menu scene
         SoundManager.instance.PlaySound(SoundEffect.menuSelect, GameData.menuSelectVolume);
         Score.playerScore = 0;
+        Application.LoadLevel("MainMenu");
+    }
+
+    // Goes to the Customization Screen
+    public void NewGame()
+    {
+        SoundManager.instance.PlaySound(SoundEffect.menuSelect, GameData.menuSelectVolume);
         Application.LoadLevel("CustomizationScreen");
+    }
+
+    // Quits the game
+    public void QuitGame()
+    {
+        SoundManager.instance.PlaySound(SoundEffect.menuSelect, GameData.menuSelectVolume);
+        Application.Quit();
     }
 }
