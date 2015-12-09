77dd5297c8be110ba3b2a8ccc0b4279b9d9a0130
gbohnen <st-it43@PICARD99.uccs.edu>
2015-12-09 09:44:10 -0700

    Help and main menu buttons added to custom scene

---
+++ RPGesqueShooter/Assets/Scripts/Design/CustomizationMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomizationMenu : MonoBehaviour {
+
+	public GameObject helpPanel;
+
+
+	public void HelpMenu()
+	{
+		if (helpPanel.GetComponent<CanvasGroup> ().blocksRaycasts == false) {
+			helpPanel.GetComponent<CanvasGroup> ().alpha = 1;
+			helpPanel.GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		} else {
+			helpPanel.GetComponent<CanvasGroup> ().alpha = 0;
+			helpPanel.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		}
+	}
+
+	public void MainMenu()
+	{
+		Application.LoadLevel("MainMenu");
+	}
+}
